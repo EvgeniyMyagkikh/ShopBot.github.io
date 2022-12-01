8e2afadd1d15b55c073232f7dc4530570d928502
EvgeniyMyagkikh <90198862+EvgeniyMyagkikh@users.noreply.github.com>
2022-12-01 23:58:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,23 +7,82 @@ tg.MainButton.color = "#2cab37";
 
 let item = "";
 
-for (let i = 1; i < 9; i++){
-var btn = document.getElementById(String(i))};
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
 
-btn.addEventListener("click", function(){
-  if (tg.MainButton.isVisible) {
-    tg.MainButton.hide();
-  }
-  else {
-    tg.MainButton.setText("Вы выбрали товар");
-    item = String(i);
-    tg.MainButton.show();
-  }
+btn1.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 1!");
+		item = "1";
+		tg.MainButton.show();
+	}
+});
+
+btn2.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 2!");
+		item = "2";
+		tg.MainButton.show();
+	}
+});
+
+btn3.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 3!");
+		item = "3";
+		tg.MainButton.show();
+	}
+});
+
+btn4.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 4!");
+		item = "4";
+		tg.MainButton.show();
+	}
+});
+
+btn5.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 5!");
+		item = "5";
+		tg.MainButton.show();
+	}
+});
+
+btn6.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 6!");
+		item = "6";
+		tg.MainButton.show();
+	}
 });
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-  tg.sendData(item);
+	tg.sendData(item);
 });
 
 let usercard = document.getElementById("usercard");
@@ -34,7 +93,7 @@ p.innerText = `${tg.initDataUnsafe.user.first_name}
 ${tg.initDataUnsafe.user.last_name}`;
 
 
-usercard.appendChild(p);
+usercard.appendChild(p); 
 
 
 
