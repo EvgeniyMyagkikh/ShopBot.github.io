9602bdd5e2d1cf2de6b04c23a9e40aefac3a6c48
EvgeniyMyagkikh <90198862+EvgeniyMyagkikh@users.noreply.github.com>
2022-12-03 15:45:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ tg.MainButton.color = "#2cab37";
 let item = "";
 
 // получили все кнопки
-const inner = document.querySelector(".inner");
+const inner = document.querySelector(".item");
 
 // действие кнопки
 function show_message(inner){
@@ -16,22 +16,22 @@ function show_message(inner){
         tg.MainButton.hide();
     }
     else {
-        tg.MainButton.setText("Вы выбрали " + alert(inner.getAttribute("alt"));
-        item = '1';
+        tg.MainButton.setText("Вы выбрали " + alert(item.getAttribute("alt"));
+        litem = '1';
         tg.MainButton.show();
     }
 }
 
 // проверка, что это кнопка
 inner.addEventListener("click", function(event){
-    if (event.target.closest('.item')){
+    if (event.target.closest('.btn')){
     show_message();
     }
  });
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-  tg.sendData(item);
+  tg.sendData(litem);
 });
 
 let usercard = document.getElementById("usercard");
