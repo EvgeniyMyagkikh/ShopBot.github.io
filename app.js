c2b08fcdfcf2917f2ab1160ae404d10d576addac
EvgeniyMyagkikh <90198862+EvgeniyMyagkikh@users.noreply.github.com>
2022-12-03 23:51:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,29 +9,47 @@ let item = "";
 
 // Получили все объекты .inner
 const inner = document.querySelector(".inner");
+const data_count = document.querySelectorAll('[data-count]')
+if (data_count) {
+  data_count.forEach(data_count => {
+    data_count.addEventListener('click', e => {
+      const target = e.target
+
+      if (target.closest('.counter__btn_minus')) {
+        let value = parseInt(target.closest('.counter').querySelector('input').value);
+        --value;
+      }
+      if (target.closest('.counter__btn_plus')) {
+        let value = parseInt(target.closest('.counter').querySelector('input').value);
+        value++;
+      }
+      target.closest('.counter').querySelector('input').value = value;
+    })
+  })
+}
+
 
 // Отправка сообщения, что на кнопку нажали
-function show_message(id){
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    }
-    else {
-        tg.MainButton.setText("Вы выбрали " + id)
-        item = '1';
-        tg.MainButton.show();
-    }
+function show_message(id) {
+  if (tg.MainButton.isVisible) {
+    tg.MainButton.hide();
+  } else {
+    tg.MainButton.setText("Перейти к оплате " + id)
+    item = '1';
+    tg.MainButton.show();
+  }
 }
 
 // Проверка, что объект - кнопка
-inner.addEventListener("click", function(event){
-    if (event.target.closest('.btn')){
+inner.addEventListener("click", function (event) {
+  if (event.target.closest('.btn')) {
     id = event.target.getAttribute('id')
     show_message(id);
-    }
- });
+  }
+});
 
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
   tg.sendData(item);
 });
 
