9d09c20b869b053545856b10e5502bea9709de98
EvgeniyMyagkikh <90198862+EvgeniyMyagkikh@users.noreply.github.com>
2022-12-04 21:39:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,29 +9,6 @@ let item = "";
 
 // Получили все объекты .inner
 const inner = document.querySelector(".inner");
-const data_count = document.querySelectorAll('[data-count]');
-
-//обработка нажатий кнопок +-
-if (data_count) {
-  data_count.forEach(data_count => {
-    data_count.addEventListener('click', e => {
-      const target = e.target
-      if (target.closest('.counter__btn_minus')) {
-        let value = parseInt(target.closest('.counter').querySelector('input').value);
-        if (value != 1) {
-          --value;
-        }
-        target.closest('.counter').querySelector('input').value = value;
-      }
-      if (target.closest('.counter__btn_plus')) {
-        let value = parseInt(target.closest('.counter').querySelector('input').value);
-        value++;
-        target.closest('.counter').querySelector('input').value = value;
-      }
-    })
-  })
-}
-
 
 // Обработка кнопки заказать
 function show_message(id) {
@@ -40,10 +17,9 @@ function show_message(id) {
   tg.MainButton.show();
 }
 
-
 // Проверка, что объект заказать - кнопка
 inner.addEventListener("click", function (event) {
-  if (event.target.closest('.btn1') || (event.target.closest('.btn2'))){
+  if (event.target.closest('.btn')){
     let id = event.target.getAttribute('id');
     show_message(id);
   }
