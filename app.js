7a0870c712e9d5ec231ef4dbcb975e619af3b655
EvgeniyMyagkikh <90198862+EvgeniyMyagkikh@users.noreply.github.com>
2022-12-04 23:44:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function show_message() {
 inner.addEventListener("click", function (event) {
   if (event.target.closest('.btn')){
     let id = event.target.getAttribute('id');
-    show_message(id);
+    show_message();
   }
 });
 
